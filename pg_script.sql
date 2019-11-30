cc17d43089c7b79675f177808998ac615c27c199
mandros1 <marinandros031@gmail.com>
2019-11-30 17:42:33 +0100

    provided postgres sql script and postman collection

--- pg_script.sql
+++ pg_script.sql
@@ -1,29 +1,17 @@
--- Database: graphql
 
--- DROP DATABASE graphql;
-
-CREATE DATABASE graphql
-    WITH 
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'Croatian_Croatia.1250'
-    LC_CTYPE = 'Croatian_Croatia.1250'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
-	
-	
 CREATE TABLE users (
 	ID SERIAL PRIMARY KEY,
 	firstName VARCHAR(30) NOT NULL,
 	lastName VARCHAR(30) NOT NULL,
-	email VARCHAR(30),
+	email VARCHAR(30) UNIQUE NOT NULL,
 	age integer NOT NULL
 );
 
 INSERT INTO users (firstName, lastName, email, age)
-	VALUES ('Marin', 'Andros', 'marinandros031@gmail.com', 25), ('Ivana', 'Runje', 'ivanarunje@gmail.com', 25), ('Josip', 'Markovic', NULL, 24);
+	VALUES ('Marin', 'Andros', 'marinandros031@gmail.com', 25), ('Ivana', 'Runje', 'ivanarunje@gmail.com', 25), ('Josip', 'Markovic', 'markovic@gmail.com', 24);
 	
 SELECT * from users;
+
 
 CREATE TABLE project (
 	ID SERIAL PRIMARY KEY,
@@ -37,6 +25,9 @@ CREATE TABLE project (
 );
 
 INSERT INTO project (author_id, created_at, title, description)
-	VALUES (1, current_timestamp, 'Book of How Amazing This Author IS', 'This book is about how amazing Marin Andros is, which is a lot.');
+	VALUES (4, current_timestamp, 'Book of How Amazing This Author IS', 'This book is about how amazing Marin Andros is, which is a lot.');
 	
 SELECT * from project;
+
+
+SELECT * FROM users LEFT JOIN project ON users.id = project.author_id WHERE project.author_id IS NULL;
